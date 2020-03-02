390cc55328fd2fb19c3b27e07ba44f25cb50f20d
datbeohbbh <phongtomfapp@gmail.com>
2020-03-03 02:51:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -94,7 +94,7 @@ namespace task{
 int main(void){
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     #ifndef ONLINE_JUDGE
-    freopen(fn".inp","r",stdin);
+    //freopen(fn".inp","r",stdin);
     //freopen(fn".out","w",stdout);
     #endif // ONLINE_JUDGE
     task::solve();
